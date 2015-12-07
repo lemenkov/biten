85083f65a99410e8f33212b9099abf0ba2e98c43
Peter Lemenkov <lemenkov@gmail.com>
2015-12-07 19:28:09 +0300

    No need to register bootstrap module
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- apps/biten/src/bootstrap.erl
+++ apps/biten/src/bootstrap.erl
@@ -14,9 +14,6 @@
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
-%% Use module name for registered process
--define(SERVER, ?MODULE).
-
 %%% ==========================================================================
 %%% API
 %%% ==========================================================================
@@ -24,7 +21,7 @@
 %% @doc Start bootstrap process and register it
 -spec start_link() -> pid().
 start_link() ->
-    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+    gen_server:start_link(?MODULE, [], []).
 
 %%% ==========================================================================
 %%% gen_server callbacks.
@@ -34,7 +31,7 @@ start_link() ->
 -record(state, {none}).
 
 init([]) ->
-    gen_server:cast(?SERVER, go),
+    gen_server:cast(self(), go),
     {ok, #state{}}.
 
 handle_call(_Request, _From, S) ->
